0fe6b494380f265ccf75e4dfebb1eebebb9d9e3e
jeffboyanich <jsboyanich123@gmail.com>
2020-11-13 22:02:15 -0800

    updated styling

--- book.js
+++ book.js
@@ -16,18 +16,17 @@ function addToLibrary(book) {
     return book;
 };
 
-const readyPlayerOne = new book('Ready Player One', 'Ernest Cline', 374, 'I have read this book');
+/*const readyPlayerOne = new book('Ready Player One', 'Ernest Cline', 374, 'I have read this book');
 addToLibrary(readyPlayerOne);
 
 const fahrenheit = new book('Fahrenheit 451', 'Ray Bradbury', 237, 'I have read this book');
 addToLibrary(fahrenheit);
 
 const HarryPotterAndTheGobletOfFire = new book('Harry Potter and The Goblet of Fire', 'J.K. Rowling', 562, 'I have read this book');
-addToLibrary(HarryPotterAndTheGobletOfFire);
+addToLibrary(HarryPotterAndTheGobletOfFire);*/
 
 console.table(myLibrary);
 
-//const deleteBtn = document.createElement('button');
 
 const form = document.getElementById('form');
 const libraryDisplay = document.getElementById('libraryContainer');
@@ -36,6 +35,15 @@ function submitBookToLibrary(title, author, pages, haveRead) {
     let newBook = new book(title, author, pages, haveRead);
     addToLibrary(newBook);
 }
+
+function getLibraryFromLocalStorage() {
+    for (i = window.localStorage.length - 1; i >= 0; i--) {
+        myLibrary.push(JSON.parse(window.localStorage.getItem(`${i}`)));
+    }
+  window.localStorage.clear();
+}
+getLibraryFromLocalStorage();
+window.localStorage.clear();
 
 function showForm() {
     form.style.display = 'block';
@@ -76,15 +84,9 @@ submitBtn.addEventListener('click', function() {
     showLibraryDisplay();
 })
 
-
-document.querySelectorAll('.deleteBtn').forEach(function() {
-    addEventListener('click', function(e) {
-        newCard.remove();
-    })
-})
-
 function removeBookFromLibrary(index) {
     myLibrary.splice(index, 1);
+    window.localStorage.clear();
     render();
 }
 function render() {
@@ -92,26 +94,33 @@ function render() {
 
     for (i = myLibrary.length - 1; i >= 0; i--) {
         let newCard = document.createElement('div');
+        newCard.classList.add('bookCard');
+        let newInfoWrapper = document.createElement('div');
+        newInfoWrapper.classList.add('cardWrapper');
         libraryDisplay.appendChild(newCard);
         let cardTitle = document.createElement('div');
+        cardTitle.classList.add('cardTitle');
         cardTitle.textContent = myLibrary[i].title;
         let cardInfo = document.createElement('ul');
+        cardInfo.classList.add('cardInfo');
         let authorInfo = document.createElement('li');
-        authorInfo.textContent = myLibrary[i].author;
+        authorInfo.textContent = 'Author: ' + myLibrary[i].author;
         cardInfo.appendChild(authorInfo);
         let pagesInfo = document.createElement('li');
-        pagesInfo.textContent = myLibrary[i].pages;
+        pagesInfo.textContent = 'Pages: ' + myLibrary[i].pages;
         cardInfo.appendChild(pagesInfo);
         let readInfo = document.createElement('li');
         readInfo.textContent = myLibrary[i].haveRead;
         cardInfo.appendChild(readInfo);
-        newCard.appendChild(cardTitle);
-        newCard.appendChild(cardInfo);
+        newInfoWrapper.appendChild(cardTitle);
+        newInfoWrapper.appendChild(cardInfo);
+        newCard.appendChild(newInfoWrapper);
         let deleteBtn = document.createElement('button');
         deleteBtn.textContent = 'Delete';
         deleteBtn.classList.add('deleteBtn');
-        deleteBtn.setAttribute('data-index', `${i}`)
+        deleteBtn.setAttribute('data-index', `${i}`);
         newCard.appendChild(deleteBtn);
+        window.localStorage.setItem(`${i}`, JSON.stringify(myLibrary[i]));
       }
       const deleteBtns = document.querySelectorAll('.deleteBtn');
       deleteBtns.forEach(function(button) {
